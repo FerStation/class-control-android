45690d12851aa4a0d140352c8b4e6d6e3a2e2e47
Fernando Bartholomeu Reis da Silva Cunha <fernandobrscunha@gmail.com>
2019-10-17 15:07:35 -0300

    add progressbar animation

--- app/src/main/java/br/com/fernandobrscunha/classcontrol/MainActivity.kt
+++ app/src/main/java/br/com/fernandobrscunha/classcontrol/MainActivity.kt
@@ -1,12 +1,33 @@
 package br.com.fernandobrscunha.classcontrol
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ProgressBar
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.YearMonth
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+   private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        progressBar=  findViewById(R.id.progressBarMonth)
+
+        progressBarAnimation()
+
+    }
+
+    private fun progressBarAnimation() {
+        progressBar.max = Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH)
+
+        var progressValue: Int = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        ObjectAnimator.ofInt(progressBar, "progress",progressValue).setDuration(500).start()
     }
 }
